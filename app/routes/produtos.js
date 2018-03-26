8393e04b20cdfec098d6ae84e78c42aed7027b77
karinafigueira <karinaottofigueira@gmail.com>
2018-03-26 17:38:14 -0300

    varbos http

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -24,4 +24,17 @@ app.get('/produtos/form', function(req, res){
   res.render('produtos/form');
 
 });
+
+app.post('/produtos/salva', function(req, res){
+
+  var produto = req.body;
+
+  var connection = app.infra.connectionFactory();
+  var produtosDAO = new app.infra.ProdutosDAO(connection);
+  produtosDAO.salva(produto, function(erros, resultados){
+    res.redirect('/produtos');
+
+
+  });
+});
 }
