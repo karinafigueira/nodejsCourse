3b5225895e62776d9cd0c97d43cf261b1d6cf3e0
karinafigueira <karinaottofigueira@gmail.com>
2018-04-02 10:15:19 -0300

    Respeitando o accept

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,13 +1,22 @@
 module.exports = function(app){
   app.get('/produtos', function(req, res){
+
     var connection = app.infra.connectionFactory();
     var produtoDAO = new app.infra.ProdutosDAO(connection);
 
   produtoDAO.lista(function(erros, resultados){
-    res.render('produtos/lista', {lista:resultados});
+    res.format({
+      html: function(){
+        res.render('produtos/lista', {lista:resultados});
+      },
+      json: function(){
+        res.json(resultados);
+      }
+    });
   });
   connection.end();
 });
+
 
 app.get('produtos/remove', function(){
 
@@ -21,20 +30,38 @@ app.get('produtos/remove', function(){
 });
 
 app.get('/produtos/form', function(req, res){
-  res.render('produtos/form');
+  res.render('produtos/form', {errosValidacao:{}, produto: {}});
 
 });
 
 app.post('/produtos/salva', function(req, res){
 
   var produto = req.body;
+  req.assert('titulo', 'Título é obrigatório').notEmpty();
+  req.assert('preco', 'Formato inválido').isFloat();
+  var erros = req.validationErrors();
+       if(erros) {
+           res.format({
+               html: function() {
+                   res.status(400).render('produtos/form', {errosValidacao:erros, produto:produto});
+               },
+               json: function() {
+                   res.status(400).json(errosValidacao);
+               }
+           });
+
+           return;
+       }
+
 
   var connection = app.infra.connectionFactory();
   var produtosDAO = new app.infra.ProdutosDAO(connection);
   produtosDAO.salva(produto, function(erros, resultados){
+    console.log(erros);
     res.redirect('/produtos');
 
 
   });
+  connection.end();
 });
 }
---
+++ cadastra-livros-terminal.js
@@ -0,0 +1,30 @@
+var http = require('http');
+
+var configuracoes = {
+  hostname: 'localhost',
+  port:3000,
+  path: '/produtos',
+  method: 'post',
+  headers: {
+    'Accept':'application/json',
+    'Content-type': 'application/json'
+
+  }
+};
+
+var client = http.request(configuracoes, function(res){
+  console.log(res.statusCode);
+  res.on('data',function(body){
+
+    console.log('Corpo: ' + body);
+  });
+
+});
+
+var produto = {
+  título: '',
+  descricao: '2',
+  preco: '100'
+};
+
+client.end(JSON.stringify(produto));
